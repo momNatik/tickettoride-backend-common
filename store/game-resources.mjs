95c8580b1d504ce10fdd3d2eaa7e088e3c95e497
Nataliya <mom.natik@gmail.com>
2025-08-02 17:19:02 +0300

    refactoring

--- store/game-resources.mjs
+++ store/game-resources.mjs
@@ -22,8 +22,27 @@ export async function CheckHealthAsync() {
     }
   }
 
+export async function CreateAsync(game) {
+  return await ExecuteOnDB(async collection => await CreateOperationAsync(collection, game));
+}
+
+export async function UpdateAsync(game) {
+  return await ExecuteOnDB(async collection => await UpdateOperationAsync(collection, game));
+}
+
 export async function GetStatusAsync(gameId) {
   return await ExecuteOnDB(async collection => await GetStatusOperationAsync(collection, gameId));
+}
+
+async function CreateOperationAsync(collection, game) {
+  return await collection.insertOne(game);
+}
+
+async function UpdateOperationAsync(collection, game) {
+  const query = { id: game.id };
+  const update = { $set: game };
+
+  return await collection.updateOne(query, update);
 }
 
 async function GetStatusOperationAsync(collection, gameId) {
