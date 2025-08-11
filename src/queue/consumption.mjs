5cb654751760b174bb6f95f0f740e66be4898389
Nataliya <mom.natik@gmail.com>
2025-08-11 18:53:00 +0300

    + consumption

---
+++ src/queue/consumption.mjs
@@ -0,0 +1,45 @@
+import amqp from "amqplib";
+
+export async function ConnectToQueue(options, handler) {
+  const opt = {
+    credentials: amqp.credentials.plain(
+      process.env.RABBIT_MQ_USER,
+      process.env.RABBIT_MQ_PW
+    ),
+  };
+
+  const connection = await amqp.connect(process.env.RABBIT_MQ_URL, opt);
+  const channel = await connection.createChannel();
+
+  await channel.assertExchange(
+    options.exchangeName,
+    process.env.EXCHANGE_TYPE,
+    {
+      durable: false,
+    }
+  );
+
+  const queue = await channel.assertQueue(options.queueName, {
+    exclusive: true,
+  });
+
+  console.log("Waiting for messages....");
+
+  // binding the queue
+  const binding_key = "";
+  channel.bindQueue(queue.queue, options.exchangeName, binding_key);
+
+  console.log("consuming messages from queue: ", queue.queue);
+
+  channel.consume(queue.queue, async (msg) => {
+    if (msg.content) {
+      const msgData = JSON.parse(msg.content);
+
+      console.log("Received object: ", JSON.stringify(msgData));
+
+      await handler(msgData);
+    }
+
+    channel.ack(msg);
+  });
+}
