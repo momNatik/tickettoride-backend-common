e7ef3d11d1c59ac4ea146592ae3c9f00b771db0d
Nataliya <mom.natik@gmail.com>
2025-08-15 12:51:56 +0300

    cleanup

--- src/db/provider.mjs
+++ src/db/provider.mjs
@@ -1,10 +1,12 @@
 import { CreateAsync, GetAsync } from "./repository.mjs";
 
-export async function CreateGameAsync(game) {
+export default { CreateGameAsync, IsResourcesReadyAsync }
+
+async function CreateGameAsync(game) {
     await CreateAsync(game);
 }
 
-export async function IsResourcesReadyAsync(gameId) {
+async function IsResourcesReadyAsync(gameId) {
     const game = await GetAsync(gameId);
     return game?.isResourcesReady === true;
 }
